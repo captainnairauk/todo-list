512ca6185e7c0c6f9986276e84a142c41a52f7dd
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2025-10-18 23:00:22 +0530

    Create Todo class

---
+++ src/todo-class.js
@@ -0,0 +1,20 @@
+class Todo{
+    #completed = false;
+
+    construction(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    get completed(){
+        return this.#completed;
+    }
+
+    toggleComplete(){
+        this.#completed = !this.#completed;
+    }
+}
+
+export default Todo;
