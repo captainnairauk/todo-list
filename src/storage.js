94e04e0c32155eb0f28edcbd5851250dde518728
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2025-10-19 00:07:25 +0530

    Create Storage class

---
+++ src/storage.js
@@ -0,0 +1,38 @@
+class Storage {
+  get(key) {
+    try {
+      const serializedData = localStorage.getItem(key);
+      return serializedData ? JSON.parse(serializedData) : null;
+    } catch (error) {
+      console.error(`Error getting item from storage: ${error}`);
+      return null;
+    }
+  }
+
+  set(key, data) {
+    try {
+      const serializedData = JSON.stringify(data);
+      localStorage.setItem(key, serializedData);
+    } catch (error) {
+      console.error(`Error setting item in storage: ${error}`);
+    }
+  }
+
+  remove(key) {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error(`Error removing item from storage: ${error}`);
+    }
+  }
+
+  clear() {
+    try {
+      localStorage.clear();
+    } catch (error) {
+      console.error(`Error clearing storage: ${error}`);
+    }
+  }
+}
+
+export { Storage };
